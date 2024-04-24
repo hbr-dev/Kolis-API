f058b303fcb045acf673416b78aefcbf8fcd2486
Houssem Harbawi <houssem.harbawi@smarteo.tn>
2024-04-24 19:48:59 +0000

    fix/ TripManager

--- src/Manager/TripManager.php
+++ src/Manager/TripManager.php
@@ -144,7 +144,7 @@ class TripManager extends AbstractManager
         $criteria = [
             "date" => [
                 '<',
-                $today->format('Y-m-d')
+                $today->format('Y-m-d H:i:sO')
             ]
         ];
 

5f218e39443ea374a247faefb4524fc391bc0ad4
Houssem Harbawi <houssem.harbawi@smarteo.tn>
2024-04-28 20:47:30 +0100

    fix/ Pack controller

---
+++ src/Controller/Back/PackController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Manager\PackManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use SSH\MyJwtBundle\Annotations\Mapping;
+
+#[Route("/back")]
+class PackController extends AbstractController
+{
+
+    private $manager;
+
+    /**
+     * ClientController constructor.
+     */
+    public function __construct(PackManager $manager) {
+        $this->manager = $manager;
+    }
+
+
+
+    #[IsGranted("ROLE_ADMIN")]
+    #[Route("/pack/{code}", name: "api_get_pack", methods: ["GET"])]
+    public function getPack($code)
+    {
+        return $this->manager
+                        ->init(['code' => $code])
+                        ->getPack(true);
+    }
+
+
+
+    #[IsGranted("ROLE_ADMIN")]
+    #[Route("/packs", name: "api_admin_get_all_packs", methods: ["GET"])]
+    public function getAllPacks()
+    {
+        return $this->manager
+                        ->getAllPacks();
+    }
+}

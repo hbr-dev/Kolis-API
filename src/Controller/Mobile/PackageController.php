8daf549c315714c062b11b915d212aa974bb7225
Houssem Harbawi <houssem.harbawi@smarteo.tn>
2024-06-08 12:33:46 +0100

    fix/ Get client's packages function

--- src/Controller/Mobile/PackageController.php
+++ src/Controller/Mobile/PackageController.php
@@ -56,7 +56,7 @@ class PackageController extends AbstractController
 
 
     #[IsGranted('ROLE_CLIENT')]
-    #[Route("/trips", name: "api_get_my_packages", methods: ["GET"])]
+    #[Route("/packages", name: "api_get_my_packages", methods: ["GET"])]
     public function getClientPackages() 
     {
         return $this->manager
